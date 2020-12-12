67e94c97be9029756c94020a2690f3a4e93920d6
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-12-12 14:31:06 +0200

    Teht 8 puulle lisää syvyyttä ja epäonnistuva haku

--- src/teht8/Tester.cpp
+++ src/teht8/Tester.cpp
@@ -13,11 +13,22 @@ int main(int argc, char* argv[])
         current->add("f");
         current->add("e");
 
+        current->add("o");
+        current->add("p");
+        current->add("h");
+        current->add("j");
+        current->add("k");
+        current->add("i");
+
         std::cout << "Tulostetaan puu esijärjestyksessä:" << std::endl
                   << current->PreOrder() << std::endl;
 
         if(current->search("c")) {
           std::cout << "Arvo 'c' löytyi puusta.\n";
+        }
+
+        if (!current->search("z")) {
+          std::cout << "Arvoa 'z' ei löytynyt puusta.\n";
         }
         return 0;
 }
