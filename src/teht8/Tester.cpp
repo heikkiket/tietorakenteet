9af7b4273e7577ee8f6794b955eb018445ba7217
Heikki Ketoharju <heikki.ketoharju@iki.fi>
2020-12-12 14:24:46 +0200

    Teht8 testausohjelma

--- src/teht8/Tester.cpp
+++ src/teht8/Tester.cpp
@@ -9,6 +9,15 @@ int main(int argc, char* argv[])
         right = new BinaryTree("c", NULL, right);
         left = new BinaryTree("b");
         current = new BinaryTree("a", left, right);
-        current->PreOrder();
+
+        current->add("f");
+        current->add("e");
+
+        std::cout << "Tulostetaan puu esijärjestyksessä:" << std::endl
+                  << current->PreOrder() << std::endl;
+
+        if(current->search("c")) {
+          std::cout << "Arvo 'c' löytyi puusta.\n";
+        }
         return 0;
 }
